ce37d703b0b6bc2bef7d405b6ada72f6c6076a28
vijayparmar27 <112509583+vijayparmar27@users.noreply.github.com>
2023-12-30 21:36:02 +0530

    add code

---
+++ README.md
@@ -0,0 +1,9 @@
+- pip install pipenv
+
+- pipenv install fastapi
+
+- pipenv install uvicorn 
+
+- uvicorn main:app [start server]
+
+- uvicorn main:app --reload [like nodemon]
---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+    # return "hey...!!"
+    # return {"name" : "dev"}    # retun json
+    return {"1", "2", "3"}         # retun list
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+# query parameter
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id
+    return {'data': id}
+
+# /blog/{id}/comments?limit=10&&published=False
+# /blog/{id}/comments?limit=10
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10, published: bool | None = None):
+    print(f"id : {id} and published : {published}")
+    # fetch comments of blog with id = id
+    return {'data': {'1', '2'}}
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: list[str] = []
+
+
+@app.post("/items/")
+async def create_item(item: Item) -> Item:
+    return item
+
+# if __name__ == '__main__':                          # this only for development 
+#     uvicorn.run("main:app",port=9000,reload=True)
+
+uvicorn.run(app,port=9000)
